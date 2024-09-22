c9daa6ff54baf5a0558aeb942649ba8f5f053876
Tejas Choudhary <162564764+Tejas0760@users.noreply.github.com>
2024-09-22 23:17:05 +0530

    Add files via upload

---
+++ movies/src/main/java/dev/Tejas/movies/Repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.Tejas.movies.Repository;
+
+import dev.Tejas.movies.Entity.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
